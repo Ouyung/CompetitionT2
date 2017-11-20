888be50bd3aaecaf24c60d4023dcafdd97d14563
Ou_yung <b951753963@gmail.com>
2017-11-20 11:03:28 +0800

    button

--- app/src/main/java/oliver/com/competitiontt/MainActivity.java
+++ app/src/main/java/oliver/com/competitiontt/MainActivity.java
@@ -1,9 +1,15 @@
 package oliver.com.competitiontt;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
