0de7828ce8d4e02471e9d91ba2887820d5846074
Ou_yung <b951753963@gmail.com>
2017-11-22 16:33:41 +0800

    dialog

---
+++ app/src/main/java/oliver/com/competitiontt/searchActivity.java
@@ -0,0 +1,13 @@
+package oliver.com.competitiontt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class searchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
