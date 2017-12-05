d25daee96a597bbdb68d78f808a1eb568995cd93
ouyung0215 <b951753963@gmail.com>
2017-12-05 16:28:39 +0800

    1205

--- app/src/main/java/oliver/com/competitiontt/CustomDialogActivity.java
+++ app/src/main/java/oliver/com/competitiontt/CustomDialogActivity.java
@@ -6,7 +6,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
@@ -14,15 +16,24 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class CustomDialogActivity extends Activity {
+
+    private static final String TAG = "FirebaseAuth";
 
     Button btn_confirm,btn_cancel;
     TextView text_content,text_title,text_close;
     EditText username,password;
     String USERNAME,PASSWORD;
 
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
-    private String PREF_FILE;
     Context context;
 //    EditText username,password;
 
@@ -30,60 +41,101 @@ public class CustomDialogActivity extends Activity {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_custom_dialog);
-        findView();
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
 
+
+
+        findView();
+        setListeners();
+
+
+    }
+
+
+//        btn_cancel.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(CustomDialogActivity.this, "you click cancel!", Toast.LENGTH_SHORT).show();
+//                finish();
+//            }
+//        }
+//        text_close.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(CustomDialogActivity.this, "you click close!", Toast.LENGTH_SHORT).show();
+//                finish();
+//            }
+//        }
+
+    void findView(){
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
         btn_confirm = (Button) findViewById(R.id.btn_confirm);
         btn_cancel = (Button) findViewById(R.id.btn_cancel);
         text_close = (TextView) findViewById(R.id.text_close);
         text_title = (TextView) findViewById(R.id.text_title);
         text_content = (TextView) findViewById(R.id.text_content);
 
-
-        btn_confirm.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                SharedPreferences getInfo = getSharedPreferences(PREF_FILE, MODE_PRIVATE);
-                final String NAME = getInfo.getString(USERNAME, "");
-                final String PASS = getInfo.getString(PASSWORD, "");
-
-                if (username.length() == 0 || password.length() == 0) {
-                    Toast.makeText(CustomDialogActivity.this, "欄位不可空白", Toast.LENGTH_SHORT).show();
-                } else {
-                    if (username.getText().toString().equals(NAME) && password.getText().toString().equals(PASS)) {
-                        Intent intent = new Intent();
-                        intent.setClass(CustomDialogActivity.this, UserHomePage.class);
-                        startActivity(intent);
-                        Toast.makeText(CustomDialogActivity.this, "帳號驗證成功", Toast.LENGTH_SHORT).show();
-                    } else if (!username.getText().toString().equals(NAME) || !password.getText().toString().equals(PASS)) {
-                        Toast.makeText(CustomDialogActivity.this, "帳號驗證失敗", Toast.LENGTH_SHORT).show();
-                    }
-                }
-
-                Toast.makeText(CustomDialogActivity.this, "you click confirm!", Toast.LENGTH_SHORT).show();
-                finish();
-            }
-        });
-        btn_cancel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(CustomDialogActivity.this, "you click cancel!", Toast.LENGTH_SHORT).show();
-                finish();
-            }
-        });
-        text_close.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(CustomDialogActivity.this, "you click close!", Toast.LENGTH_SHORT).show();
-                finish();
-            }
-        });
-    }
-    void findView(){
-        username = (EditText) findViewById(R.id.username);
-        password = (EditText) findViewById(R.id.password);
-        btn_confirm = (Button) findViewById(R.id.btn_confirm);
-
         context=this;
 
     }
+
+    private void setListeners() {
+        btn_confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String inputUserName = username.getText().toString();
+                String inputPassword = password.getText().toString();
+                signIn(inputUserName, inputPassword);
+            }
+        });
+    }
+    private void signIn(String email, String password) {
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(Task<AuthResult> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithEmail:failed", task.getException());
+                            Toast.makeText(CustomDialogActivity.this, "登入失敗",
+                                    Toast.LENGTH_SHORT).show();
+                        }else{
+                            Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
+                            Toast.makeText(CustomDialogActivity.this, "登入成功",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
 }
--- app/src/main/java/oliver/com/competitiontt/RegActivity.java
+++ app/src/main/java/oliver/com/competitiontt/RegActivity.java
@@ -2,62 +2,154 @@ package oliver.com.competitiontt;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 import static android.R.attr.name;
 import static oliver.com.competitiontt.R.id.password;
 
 public class RegActivity extends AppCompatActivity {
-    EditText unit, name, job, mail, password, password2;
+    private static final String TAG = "FirebaseAuth";
+    EditText unit, name, job, mail, etpassword,etpassword2;
     Button register;
     Intent intent;
     private String PREF_FILE;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
     String UNIT, NAME, JOB, MAIL, PASSWORD, PASSWORD2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reg);
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
+        findViews();
+        setListeners();
+    }
+
+    private void findViews() {
 
         unit = (EditText) findViewById(R.id.et_unit);
         name = (EditText) findViewById(R.id.et_name);
         job = (EditText) findViewById(R.id.et_job);
         mail = (EditText) findViewById(R.id.et_mail);
-        password = (EditText) findViewById(R.id.et_password);
-        password2 = (EditText) findViewById(R.id.et_password2);
+        etpassword = (EditText) findViewById(R.id.et_password);
+        etpassword2 = (EditText) findViewById(R.id.et_password2);
         register = (Button) findViewById(R.id.register);
 
+
+    }
+
+    private void setListeners() {
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (unit.length()!=0 && name.length()!=0 && job.length()!=0 && mail.length() != 0 &&  password.length() != 0 && password2.length() != 0) {
-                    Intent intent = new Intent();
-                    intent.setClass(RegActivity.this, MainActivity.class);
-                    startActivity(intent);
-                    restore();
-                } else {
-                    Toast.makeText(RegActivity.this, "欄位不可空白", Toast.LENGTH_SHORT).show();
-                }
+                String email = mail.getText().toString();
+                String password = etpassword.getText().toString();
+//                String unit1 = unit.getText().toString();
+//                String name1 = name.getText().toString();
+//                String job1 = job.getText().toString();
+
+
+
+                signUp(email,password);
             }
         });
     }
+    private void signUp(String email,String password){
+        mAuth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete( Task<AuthResult> task) {
+                        Log.d(TAG, "createUserWithEmail:onComplete:" + task.isSuccessful());
 
-    private void restore() {
-        SharedPreferences settings = getSharedPreferences(PREF_FILE, MODE_PRIVATE);
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(RegActivity.this, "註冊失敗",
+                                    Toast.LENGTH_SHORT).show();
+                        }else{
+                            Toast.makeText(RegActivity.this, "註冊成功",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+
+                        // ...
+                    }
+                });
+
+    }
+
+//        register.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if (unit.length()!=0 && name.length()!=0 && job.length()!=0 && mail.length() != 0 &&  password.length() != 0 && password2.length() != 0) {
+//                    Intent intent = new Intent();
+//                    intent.setClass(RegActivity.this, MainActivity.class);
+//                    startActivity(intent);
+//                    restore();
+//                } else {
+//                    Toast.makeText(RegActivity.this, "欄位不可空白", Toast.LENGTH_SHORT).show();
+//                }
+//            }
+//        });
+
+//
+//    private void restore() {
+//        SharedPreferences settings = getSharedPreferences(PREF_FILE, MODE_PRIVATE);
+//
+//
+//        settings.edit().putString(UNIT, unit.getText().toString()).commit();
+//        settings.edit().putString(NAME, name.getText().toString()).commit();
+//        settings.edit().putString(JOB, job.getText().toString()).commit();
+//        settings.edit().putString(MAIL, mail.getText().toString()).commit();
+//        settings.edit().putString(PASSWORD, password.getText().toString()).commit();
+//        settings.edit().putString(PASSWORD2, password2.getText().toString()).commit();
+//        Toast.makeText(RegActivity.this, "註冊成功", Toast.LENGTH_SHORT).show();
+//    }
 
 
-        settings.edit().putString(UNIT, unit.getText().toString()).commit();
-        settings.edit().putString(NAME, name.getText().toString()).commit();
-        settings.edit().putString(JOB, job.getText().toString()).commit();
-        settings.edit().putString(MAIL, mail.getText().toString()).commit();
-        settings.edit().putString(PASSWORD, password.getText().toString()).commit();
-        settings.edit().putString(PASSWORD2, password2.getText().toString()).commit();
-        Toast.makeText(RegActivity.this, "註冊成功", Toast.LENGTH_SHORT).show();
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+
     }
 }
 
